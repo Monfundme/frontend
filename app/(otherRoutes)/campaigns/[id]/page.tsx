83e27982e691318c1b352cee9dbb0f4d05c808cd
gwilldan <sayhi2godswill@gmail.com>
2025-03-06 11:05:06 +0100

    fix

--- app/(otherRoutes)/campaigns/[id]/page.tsx
+++ app/(otherRoutes)/campaigns/[id]/page.tsx
@@ -4,18 +4,19 @@ import { Main } from "@/components/campaigns";
 type Params = Promise<{ id: string }>;
 
 const Funding = async ({ params }: { params: Params }) => {
-	const id = (await params).id;
+  const id = (await params).id;
 
-	return (
-		<div className="  py-[120px] px-3 min-h-dvh ">
-			<Suspense
-				fallback={
-					<div className=" grid place-content-center h-dvh ">Loading...</div>
-				}>
-				<Main id={id} />
-			</Suspense>
-		</div>
-	);
+  return (
+    <div className="  py-[120px] px-3 min-h-dvh ">
+      <Suspense
+        fallback={
+          <div className=" grid place-content-center h-dvh ">Loading...</div>
+        }
+      >
+        <Main id={id} />
+      </Suspense>
+    </div>
+  );
 };
 
 export default Funding;
--- app/(otherRoutes)/campaigns/page.tsx
+++ app/(otherRoutes)/campaigns/page.tsx
@@ -1,19 +1,18 @@
-"use client";
 import { GetCampaigns, SearchForm } from "@/components/campaigns";
 import { StartButton } from "@/components/general";
 import { Suspense } from "react";
 
-const Page = () => {
-	return (
-		<div className=" py-[100px] lg:pt-[125px] p-3 min-h-dvh ">
-			<main className=" width_to_center ">
-				<h2 className=" text-[35px] font-extrabold  text-center">
-					Find fundraising campaigns
-				</h2>
-				<p className=" text-center">
-					Search for campaigns by creator&apos;s address, funding category or
-					keyword
-				</p>
+const page = () => {
+  return (
+    <div className=" py-[100px] lg:pt-[125px] p-3 ">
+      <main className=" width_to_center ">
+        <h2 className=" text-[35px] font-extrabold  text-center">
+          Find fundraising campaigns
+        </h2>
+        <p className=" text-center">
+          Search for campaigns by creator&apos;s address, funding category or
+          keyword
+        </p>
 
 				<Suspense
 					fallback={
@@ -22,6 +21,7 @@ const Page = () => {
 						</div>
 					}>
 					<SearchForm />
+
 					<section>
 						<div className=" flex items-center justify-between my-4">
 							<h2 className="text-2xl font-semibold my-2 ">Active campaigns</h2>
@@ -35,4 +35,4 @@ const Page = () => {
 	);
 };
 
-export default Page;
+export default page;
